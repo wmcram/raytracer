527093f2213ec273ada0708d6ecb2ffdc6acd485
Wil Cram <wcram@wisc.edu>
2025-07-15 00:26:47 -0500

    Add vec3 and color

--- main.cpp
+++ main.cpp
@@ -1,25 +1,22 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>
 
 int main() {
-    int image_width = 256;
-    int image_height = 256;
-    
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+  int image_width = 256;
+  int image_height = 256;
 
-    for (int i = 0; i < image_height; i++) {
-        std::clog << "\rScanlines remaining: " << (image_height - i) << std::flush;
-        for (int j = 0; j < image_width; j++) {
-            double r = double(j) / (image_width-1);
-            double g = double(i) / (image_height-1);
-            double b = 0.0;
+  std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
-        }
+  for (int i = 0; i < image_height; i++) {
+    std::clog << "\rScanlines remaining: " << (image_height - i) << std::flush;
+    for (int j = 0; j < image_width; j++) {
+      color pixel_color = color(double(i) / (image_width - 1),
+                                double(j) / (image_height - 1), 0);
+      write_color(std::cout, pixel_color);
     }
+  }
 
-    std::clog << "\rDone.        \n";
+  std::clog << "\rDone.        \n";
 }
